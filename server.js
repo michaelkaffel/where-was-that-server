90c265288386351fe1ef818fe4b9d3a78dd140ed
MichaelKaffel <michaeldkaffel@gmail.com>
2026-02-04 15:52:15 -0800

    Express and morgan middleware installed

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const morgan = require('morgan');
 
@@ -9,3 +10,13 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server for <a href="https://where-was-that-mk-react.web.app/">Where Was That</a></h1></body></html>');
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+});
